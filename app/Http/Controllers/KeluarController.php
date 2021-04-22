eb88299c7a3c0bb38fdc9d42f8edd24ba9053ee6
Prianugrah Widijatmiko <35441939+priawidi@users.noreply.github.com>
2021-04-22 12:38:34 +0700

    tambah addkeluar

--- app/Http/Controllers/KeluarController.php
+++ app/Http/Controllers/KeluarController.php
@@ -39,14 +39,25 @@ class KeluarController extends Controller
     public function store(Request $request)
     {
         $surat_keluar = new Keluar;
-        $surat_keluar->nomor_agenda = ucwords(strtolower($request->nomor_agenda));
-        $surat_keluar->tgl_masuk = ucwords(strtolower($request->tgl_masuk));
-        $surat_keluar->untuk = ucwords(strtolower($request->untuk));
-        $surat_keluar->nomor_surat = ucwords(strtolower($request->nomor_surat));
-        $surat_keluar->tgl_surat = ucwords(strtolower($request->tgl_surat));
-        $surat_keluar->perihal_surat = ucwords(strtolower($request->perihal_surat));
-        $surat_keluar->tujuan = ucwords(strtolower($request->tujuan));
-        $surat_keluar->ket = ucwords(strtolower($request->ket));
+        $surat_keluar->nomor_agenda = ($request->nomor_agenda);
+        $surat_keluar->tgl_masuk = ($request->tgl_masuk);
+        $surat_keluar->untuk = ($request->untuk);
+        $surat_keluar->nomor_surat = ($request->nomor_surat);
+        $surat_keluar->tgl_surat = ($request->tgl_surat);
+        $surat_keluar->perihal_surat = ($request->perihal_surat);
+        $surat_keluar->tujuan = ($request->tujuan);
+        $surat_keluar->ket = ($request->ket);
+        $surat_keluar->dokumen = ($request->dokumen);
+
+        $rules = array(
+            'dokumen' => 'required|mimes:pdf'
+            );
+
+        $dokumen = $request->file('dokumen');
+        $tujuan_upload = 'document-upload';
+        $dokumen->move($tujuan_upload, $dokumen->getClientOriginalName());
+        $surat_keluar->dokumen = $dokumen->getClientOriginalName();
+       
         $simpan = $surat_keluar->save();
  
          return redirect('keluar');
@@ -64,6 +75,12 @@ class KeluarController extends Controller
         //
     }
 
+
+    public function download($dokumen)
+    {
+        return response()->download('document-upload/'.$dokumen);
+    }
+
     /**
      * Show the form for editing the specified resource.
      *
@@ -72,7 +89,8 @@ class KeluarController extends Controller
      */
     public function edit($id)
     {
-        //$surat_keluar = 
+        $surat_keluar = Keluar::find($id);
+        return view('surat.editkeluar',compact('surat_keluar'));
     }
 
     /**
@@ -84,7 +102,50 @@ class KeluarController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'nomor_agenda' => 'required',
+            'tgl_masuk' => 'required',
+            'dari' => 'required',
+            'nomor_surat' => 'required',
+            'tgl_surat' => 'required',
+            'perihal_surat' => 'required',
+            'tujuan' => 'required',
+            'ket' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $surat_keluar = Keluar::find($id);
+
+        $surat_keluar->update([
+        'nomor_agenda' => ($request->nomor_agenda),
+        'tgl_masuk' => ($request->tgl_masuk),
+        'untuk' => ($request->untuk),
+        'nomor_surat' => ($request->nomor_surat),
+        'tgl_surat' => ($request->tgl_surat),
+        'perihal_surat' => ($request->perihal_surat),
+        'tujuan' => ($request->tujuan),
+        'ket' => ($request->ket),
+        ]);
+
+        $dokumen = $request->file('dokumen');
+        if($dokumen)
+        {
+            $filename = $dokumen->getClientOriginalName();
+            $surat_keluar['dokumen'] = $filename;
+            $tujuan_upload = 'document-upload';
+            $dokumen->move($tujuan_upload, $dokumen->getClientOriginalName());
+            $surat_keluar->dokumen = $dokumen->getClientOriginalName();
+        }
+        
+            if ($surat_keluar->save()) {
+                return redirect('masuk')->with('success', 'Data Berhasil Diupdate');
+            } else {
+                return redirect('masuk')->with('error', 'error message');
+            }
     }
 
     /**
